b0dfea4ec6d8d2c3be98169b8f73340bf94c3026
Yousif AlOzairi <yoyo012@icloud.com>
2020-06-29 18:00:44 +0300

    small design changes

--- src/App.js
+++ src/App.js
@@ -17,11 +17,13 @@ function App() {
   ));
 
   return (
+    <div>
     <div style={styles.body}>
       <h1 style={styles.title}>Welcome to Eunoia</h1>
       <img style={styles.centeredImage} src={logo} alt="Eunoia Palm Tree" />
       <h4 style={styles.takeALook}>Take a look at our Notebooks!</h4>
       <div style={styles.list}>{notebookList}</div>
+    </div>
     </div>
   );
 }
