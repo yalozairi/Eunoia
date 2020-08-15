d4aa7b8e229bcf4a3ba92be70fcbfb0a0b860aab
Yousif Alozairi <Yousif@Ouss-MBP.localdomain>
2020-08-16 00:39:35 +0300

    fix instance

--- src/stores/instance.js
+++ src/stores/instance.js
@@ -1,7 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8000"
-})
+  // baseURL: "http://localhost:8000"
+  baseURL: "https://sheltered-hollows-72913.herokuapp.com",
+});
 
-export default instance
+export default instance;
